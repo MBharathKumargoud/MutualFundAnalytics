38eb294010a86b90a32934265f25bfa1066e309f
M.Bharath <25r25a0505@mlrit.ac.in>
2026-06-22 15:37:28 +0530

    Day 1: Data ingestion complete

--- data_ingestion.py
+++ data_ingestion.py
@@ -1,1 +1,40 @@
-print("hi");
+import pandas as pd
+
+# Load data
+df = pd.read_csv("data/raw/125497_nav.csv")
+
+# Convert date column
+df["date"] = pd.to_datetime(df["date"], format="%d-%m-%Y")
+
+# Sort oldest to newest
+df = df.sort_values("date")
+
+# Daily return %
+df["daily_return"] = df["nav"].pct_change() * 100
+
+# Save processed data
+df.to_csv("data/processed/nav_processed.csv", index=False)
+
+print(df.head())
+print("\nProcessed data saved successfully!")
+
+# CAGR
+start_nav = df["nav"].iloc[0]
+end_nav = df["nav"].iloc[-1]
+
+years = (df["date"].iloc[-1] - df["date"].iloc[0]).days / 365.25
+
+cagr = ((end_nav / start_nav) ** (1 / years) - 1) * 100
+
+# Volatility
+volatility = df["daily_return"].std() * (252 ** 0.5)
+
+# Drawdown
+df["cum_max"] = df["nav"].cummax()
+df["drawdown"] = (df["nav"] - df["cum_max"]) / df["cum_max"] * 100
+
+max_drawdown = df["drawdown"].min()
+
+print(f"\nCAGR: {cagr:.2f}%")
+print(f"Annualized Volatility: {volatility:.2f}%")
+print(f"Maximum Drawdown: {max_drawdown:.2f}%")
--- live_nav_fetch.py
+++ live_nav_fetch.py
@@ -1,1 +1,33 @@
-print("live_nav_fetch.py");
+import requests
+import pandas as pd
+
+scheme_codes = [
+    "125497",
+    "119551",
+    "120503",
+    "118632",
+    "119092",
+    "120841"
+]
+
+for code in scheme_codes:
+    try:
+        url = f"https://api.mfapi.in/mf/{code}"
+
+        response = requests.get(url, timeout=30)
+
+        response.raise_for_status()
+
+        data = response.json()
+
+        nav_df = pd.DataFrame(data["data"])
+
+        nav_df.to_csv(
+            f"data/raw/{code}_nav.csv",
+            index=False
+        )
+
+        print(f"{code} saved successfully")
+
+    except Exception as e:
+        print(f"Failed for {code}: {e}")
